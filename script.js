3fe009f8862cba8eff64b326ade3240e77e1779a
pkzstar <iceclimberelite@hotmail.com>
2024-07-09 22:32:29 -0500

    .

--- script.js
+++ script.js
@@ -1,4 +1,3 @@
-
 function displayUpsmash() {
 if (document.getElementById("textUpsmash").innerHTML === "HI") {
     document.getElementById("textUpsmash").innerHTML = "";
@@ -6,7 +5,7 @@ if (document.getElementById("textUpsmash").innerHTML === "HI") {
     document.getElementById("textUpsmash").innerHTML = "HI";
 }};
 
-class Upsmash {
+class kbCalc {
     constructor(bkb, kbg, percent, fallSpeed, weight, moveDmg) {
         this.bkb = bkb,
         this.kbg = kbg,
@@ -15,5 +14,14 @@ class Upsmash {
         this.weight = weight,
         this.moveDmg = moveDmg
     }
-}
+
+    kbFormula() {
+       return (this.bkb * this.percent)
+}};
+
+
+let totalKB = kbCalc;
+console.log(totalKB);
+
+
 
