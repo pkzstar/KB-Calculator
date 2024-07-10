fabde882080ea393bf59af1fc0919726306d90dc
pkzstar <iceclimberelite@hotmail.com>
2024-07-09 22:07:05 -0500

    .

---
+++ script.js
@@ -0,0 +1,19 @@
+
+function displayUpsmash() {
+if (document.getElementById("textUpsmash").innerHTML === "HI") {
+    document.getElementById("textUpsmash").innerHTML = "";
+} else {
+    document.getElementById("textUpsmash").innerHTML = "HI";
+}};
+
+class Upsmash {
+    constructor(bkb, kbg, percent, fallSpeed, weight, moveDmg) {
+        this.bkb = bkb,
+        this.kbg = kbg,
+        this.percent = percent,
+        this.fallSpeed = fallSpeed,
+        this.weight = weight,
+        this.moveDmg = moveDmg
+    }
+}
+
